d1ae11c8598fa139d9c42db32068c03f64268a24
j2hh <95945217+j2hh@users.noreply.github.com>
2023-07-06 17:47:55 +0200

    test1

---
+++ login/models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+# Eintrag DB Feld mit Text und Datum feld
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+# Eintrag DB Feld mit Frage, Antworttext und Vote
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
