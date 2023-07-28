2a399361e7d899b5078783ab1833b06716ffa4de
josch <josch.11121@gmail.com>
2023-07-28 17:28:52 +0200

    crazy 3d karte homepage done!

--- login/static/js/landing.js
+++ login/static/js/landing.js
@@ -1,43 +1,23 @@
-let card = document.querySelector('.userCard');
-let debugLine = document.querySelector('.debug-line');
-let scale = 1;
-let rotation = 0;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const pre = document.querySelector('.userCard');
 
-// Set the initial transform values
-card.style.transform = `rotateY(0deg) rotateX(0deg) scale(${scale})`;
-
-document.addEventListener('mousemove', (e) => {
-    // Get the center of the screen
-    let centerX = window.innerWidth / 2;
-    let centerY = window.innerHeight / 2;
-
-    // Calculate rotation values based on the center of the screen
-    let xAxis = (e.pageX - centerX) / 18;
-    let yAxis = -(e.pageY - centerY) / 18;
-
-    card.style.transform = `rotateY(${xAxis + rotation}deg) rotateX(${yAxis}deg) scale(${scale})`;
-
-    // Calculate the angle for the debug line
-    let angle = Math.atan2(e.pageY - centerY, e.pageX - centerX) * 180 / Math.PI;
-    debugLine.style.transform = `rotate(${angle}deg)`;
+    document.addEventListener('mousemove', (e) => {
+        rotateElement(e, pre);
+    });
 });
 
-document.addEventListener('mouseleave', (e) => {
-    card.style.transform = `rotateY(${rotation}deg) rotateX(0deg) scale(${scale})`;
-});
+function rotateElement(event, element) {
+    const x = event.clientX;
+    const y = event.clientY;
 
-document.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    scale += e.deltaY * -0.001;
-    scale = Math.min(Math.max(.125, scale), 4);
+    //Mitte herausfinden
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
 
-    card.style.transform = `rotateY(${rotation}deg) rotateX(0deg) scale(${scale})`;
-});
+    //Offset berechnen
+    const offsetX = ((x - middleX) / middleX) * 15;
+    const offsetY = ((y - middleY) / middleY) * 15;
 
-// Rotate the card when the left arrow key is pressed
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowLeft') {
-        rotation += 360; // Add 360 degrees to the rotation
-        card.style.transform = `rotateY(${rotation}deg) rotateX(0deg) scale(${scale})`;
-    }
-});
+    element.style.setProperty("--rotateY", offsetX + "deg");
+    element.style.setProperty("--rotateX", -1 * offsetY + "deg");
+}
